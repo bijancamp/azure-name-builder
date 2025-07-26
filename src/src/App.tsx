8b5f2e7a6c0b2d1406b946e74f8e3c583fed4356
Bijan "Biji" Camp <8800015+bijancamp@users.noreply.github.com>
2025-07-26 10:09:31 -0500

    Install Fluent UI React v9

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,9 +1,14 @@
+import {
+  FluentProvider,
+  webLightTheme
+} from "@fluentui/react-components";
+
 import './App.css'
 
 function App() {
   return (
-    <>
-    </>
+    <FluentProvider theme={webLightTheme}>
+    </FluentProvider>
   );
 }
 
